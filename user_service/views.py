4cd7be7fd6438008efd3bd4b9a2ed0316cba9880
nkosi-tauro <nkosilati23@yahoo.com>
2023-06-22 14:26:03 +0200

    feat(adminview): added register form

--- user_service/views.py
+++ user_service/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+'''
+User Service views
+'''
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register_employee(request):
+    '''
+    So this will allow an admin to register an employee(new user)
+    Without having to use the django admin dashboard
+    '''
+    if request.method == 'POST':
+        # This will take the form inputs (username, password)
+        form = UserCreationForm(request.POST)
+        # This will check if the inputs are valid based on the contraints
+        if form.is_valid():
+            form.save()
+            # This will redirect back to the admin view
+            return redirect('adminview')
+    else:
+        form = UserCreationForm()
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'adminview/register_employee.html', context)
