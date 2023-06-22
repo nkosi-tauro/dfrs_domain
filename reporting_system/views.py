fc6d6d6d7b8482b97216837eb796e35e1403a04d
nkosi-tauro <nkosilati23@yahoo.com>
2023-06-22 23:03:59 +0200

    feat(templates): added employee table

--- reporting_system/views.py
+++ reporting_system/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def homeview(request):
     return render(request,'homeview/home.html')
 
 def adminview(request):
-    return render(request,'adminview/admin.html')
+    cyberdetectives = User.objects.all()
+    context = {'cyberdetectives': cyberdetectives}
+    return render(request,'adminview/admin.html', context)
